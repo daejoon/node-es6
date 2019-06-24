81f84102565fadc5ac4de39aec8a042799bee110
daejoon <kkode1911@gmail.com>
2019-06-24 10:32:10 +0900

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,8 +4,8 @@ import sayHello from "index"
 describe("index test", () => {
     describe("sayHello function", () => {
         it("should say Hello Node!", () => {
+            const str = sayHello()
 
-            const str = sayHello();
             expect(str).to.equal("Hello Node!")
         })
     })
